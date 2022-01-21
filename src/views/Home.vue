a9a9b0adca77ffb9301eccf374e9d7d5e81ae917
张仕坤 <a1157911285@163.com>
2022-01-21 21:25:01 +0800

    test:添加elementUI测试按钮

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
         <router-link to="/backend">backend</router-link>
         <router-link to="/frontend">frontend</router-link>
         <router-link to="/recommended">recommended</router-link>
+        <el-button type="primary">elementUI</el-button>
         <router-view></router-view>
     </div>
 </template>
